18daba2f2aa48affe995e97a6eb07d2195696bb2
Chiara Debenedetti <chiara.debenedetti@impact-initiatives.org>
2020-06-08 16:59:54 +0200

    new test in debugging

--- R/rcm_download.R
+++ R/rcm_download.R
@@ -34,7 +34,9 @@ rcm_download <- function(include_archived=F,include_validated=F,after_year="2015
 
   ##Chiara: comment out the check on early cycles, not necessary, since the new RCM is only referred to current RCs, and causes an error
   after_year<-as.Date(paste0(after_year,"2014-12-31"),format="%Y-%m-%d")
-  too_early_entries<-apply(rcm[,datecols],2,function(x){x<after_year}) %>% apply(1,any,na.rm =T)
+  ##chiara change here from %>% apply
+  too_early_entries<-apply(rcm[,datecols],2,function(x){x<after_year})
+  too_early_entries <- apply(too_early_entries, 1,any,na.rm =T)
   rcm<-rcm[!too_early_entries,,drop=F]
 
 
